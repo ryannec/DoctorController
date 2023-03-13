9eed8f3122a9f5f9874808b61c8680f8714d679a
DIEU Anthony (SNCF VOYAGEURS / TECHNICENTRE DE VENISSIEUX / Equ Maintenance) <8402956y@commun.ad.sncf.fr>
2023-03-13 13:59:37 +0100

    first commit

---
+++ src/main/java/com/controller/DoctorController/DoctorControllerApplication.java
@@ -0,0 +1,13 @@
+package com.controller.DoctorController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorControllerApplication.class, args);
+	}
+
+}
